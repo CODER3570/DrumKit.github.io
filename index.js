6d328715a7e28892bbe648e8d3ac71c73575844c
AbdulRehman Rizwan <98815612+CODER3570@users.noreply.github.com>
2022-12-25 19:47:17 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,296 @@
+//Detecting Button Press  and Assigning kes=ys 
+
+
+
+
+for (var i=0 ; i < document.querySelectorAll(".drum").length ; i++) {
+document.querySelectorAll(".drum")[i].addEventListener("click" , function () {
+
+var innerButtonhtml = this.innerHTML;
+makeSound(innerButtonhtml);
+addanimation(innerButtonhtml);
+
+}
+
+)};
+
+// Detecting the Key
+
+document.addEventListener('keydown' , function(event){
+
+makeSound(event.key);
+addanimation(event.key);
+
+
+
+// Running Function
+
+});
+
+function makeSound(key) {
+    switch (key) {
+    
+        case "w":
+     var tom1 = new Audio("sounds/tom-1.mp3");
+     tom1.play();
+            break;
+    
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                       break;
+    
+                       case "s":
+                        var tom3 = new Audio("sounds/tom-3.mp3");
+                        tom3.play();
+                               break;
+    
+                               case "d":
+                                var tom4 = new Audio("sounds/tom-4.mp3");
+                                tom4.play();
+                                       break;
+    
+                                       case "j":
+                                        var tom5 = new Audio("sounds/crash.mp3");
+                                        tom5.play();
+                                               break;
+    
+                                               case "k":
+                                                var tom6 = new Audio("sounds/kick-bass.mp3");
+                                                tom6.play();
+                                                       break;
+    
+                                                       case "l":
+                                                        var tom7 = new Audio("sounds/snare.mp3");
+                                                        tom7.play();
+                                                               break;
+        default: console.log(innerButtonhtml);
+
+    }
+
+}
+
+
+
+  function addanimation(currentkey) {
+    var animatButton = document.querySelector("." + currentkey) ; 
+    animatButton.classList.add("pressed");
+    setTimeout(function(){
+       animatButton.classList.remove('pressed'); 
+    },  60);
+    
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
